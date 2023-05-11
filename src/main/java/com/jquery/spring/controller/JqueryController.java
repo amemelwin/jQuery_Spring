2471f811f77e92efbb74a278263fe2439dbc64bb
Ame <mrs.amemelwin@gmail.com>
2023-05-11 22:32:02 +0630

    push

--- src/main/java/com/jquery/spring/controller/JqueryController.java
+++ src/main/java/com/jquery/spring/controller/JqueryController.java
@@ -2,6 +2,7 @@ package com.jquery.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.jquery.spring.service.ItemService;
@@ -13,8 +14,8 @@ public class JqueryController {
 	ItemService itemService;
 	
 	@GetMapping("/")
-	public String index() {
-		System.out.println(this.itemService.getItems());
+	public String index(Model model) {
+		model.addAttribute("items",this.itemService.getItems());
 		return "screen/index";
 	}
 	
